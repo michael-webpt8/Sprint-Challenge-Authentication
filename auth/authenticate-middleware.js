0b2b93464e09b0331de2b3afc0b3b794861ac767
Michael Williams <manicdesigner@gmail.com>
2020-01-25 15:00:34 -0500

    middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -5,12 +5,13 @@
 const jwt = require('jsonwebtoken');
 const secrets = require('../config/secrets');
 
-module.exports = (req, res, next) => {
+module.exports = () => {
   return (req, res, next) => {
     try {
       const token = req.headers.authorization;
-      jwt.verify(token, secrets.jwt);
+      const decode = jwt.verify(token, secrets.jwt);
 
+      req.id = decode.subject;
       next();
     } catch (err) {
       res.status(401).json({ you: 'shall not pass!' });
