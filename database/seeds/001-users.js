3d5bee3b98694bbdb30aa29161f237048ce57592
Michael Williams <manicdesigner@gmail.com>
2020-01-25 15:00:11 -0500

    adddd seed data

---
+++ database/seeds/001-users.js
@@ -0,0 +1,9 @@
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').truncate();
+
+  await knex('users').insert([
+    { username: 'jay', password: 'abc' },
+    { username: 'mik', password: '123' },
+  ]);
+};
