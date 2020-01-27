ddb04c416ab43e4692b6b1f0a644083c4ae01744
Michael Williams <manicdesigner@gmail.com>
2020-01-27 16:42:03 -0500

    fixed up spec

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,28 +1,31 @@
 const supertest = require('supertest');
 const server = require('../api/server');
 const db = require('../database/dbConfig');
-const dbModel = require('./auth-model');
+//const dbModel = require('./auth-model');
+//const bcrypt = require('bcryptjs');
 
 beforeEach(async () => {
-  db.seed.run();
+  await db.seed.run();
 });
+describe('auth router', () => {
+  test('register a user', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'mikey', password: 'abc123' });
+    //expect(res.status).toBe(201);
+    expect(res.status).toBe(201);
+    expect(res.type).toBe('application/json');
+    expect(res.body.username).toBe('mikey');
+  });
 
-test('register a user', async () => {
-  const res = await supertest(server)
-    .post('/api/auth/register')
-    .send({ username: 'mikey', password: 'abc123' });
-  //expect(res.status).toBe(201);
-  expect(res.status).toBe(201);
-  expect(res.type).toBe('application/json');
-  expect(res.body.username).toBe('mikey');
-});
-
-test('login user', async () => {
-  const res = await supertest(server)
-    .post('/api/auth/login')
-    .send({ name: 'jay', password: 'abc' });
-
-  expect(res.status).toBe(200);
-  expect(res.type).toBe('application/json');
-  expect(res.username).toBe('jay');
+  test('login user', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'mikey', password: 'abc123' });
+    //console.log('U', user);
+    //console.log('R', res);
+    expect(res.status).toBe(200);
+    expect(res.type).toBe('application/json');
+    expect(res.username).toBe('mikey');
+  });
 });
